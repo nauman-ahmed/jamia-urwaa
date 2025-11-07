5271a50da5ad422d39639ff8a6a0cdc50c12b93e
Nauman Ahmed <naumanahmed449@gmail.com>
2025-11-07 12:04:17 +0100

    added urdu

---
+++ src/admin/app.js
@@ -0,0 +1,44 @@
+const config = {
+  locales: [
+    // 'en', // English is the default language
+    'ur', // Urdu
+    'ur-IN', // Urdu
+    'ur-PK', // Urdu
+    'ar', // Arabic
+    // 'fr', // French
+    // 'cs', // Czech
+    // 'de', // German
+    // 'dk', // Danish
+    // 'es', // Spanish
+    // 'he', // Hebrew
+    // 'id', // Indonesian
+    // 'it', // Italian
+    // 'ja', // Japanese
+    // 'ko', // Korean
+    // 'ms', // Malay
+    // 'nl', // Dutch
+    // 'no', // Norwegian
+    // 'pl', // Polish
+    // 'pt-BR', // Portuguese (Brazil)
+    // 'pt', // Portuguese
+    // 'ru', // Russian
+    // 'sk', // Slovak
+    // 'sv', // Swedish
+    // 'th', // Thai
+    // 'tr', // Turkish
+    // 'uk', // Ukrainian
+    // 'vi', // Vietnamese
+    // 'zh-Hans', // Chinese (Simplified)
+    // 'zh', // Chinese
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
+
